52ff61542e2e5450a74faa985d089061a854165a
Gabriel Coelho <gabrielcpcoelho@gmail.com>
2020-10-27 14:35:27 -0300

    Personalização das mensagens de retorno de erros de validação

---
+++ src/main/java/com/gabrielcoelho/cursomc/resources/exceptions/FieldErrorMessage.java
@@ -0,0 +1,33 @@
+package com.gabrielcoelho.cursomc.resources.exceptions;
+
+public class FieldErrorMessage {
+
+	private String fieldName;
+	private String message;
+	
+	public FieldErrorMessage() {
+	}
+	
+	public FieldErrorMessage(String fieldName, String message) {
+		super();
+		this.fieldName = fieldName;
+		this.message = message;
+	}
+
+	public String getFieldName() {
+		return fieldName;
+	}
+
+	public void setFieldName(String fieldName) {
+		this.fieldName = fieldName;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
